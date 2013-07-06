2287d177c903a301731236e73835d13b8b48b826
Juan Manuel Arias <juanmanuelarias@gmail.com>
2013-07-06 15:19:25 -0300

    added an index.html file

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+var fs = require('fs');
 var express = require('express');
+
+var content;
+
+fs.readFile('index.html', 'utf-8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    content = data;
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
